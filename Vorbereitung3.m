7efe36425b397a6dfa689ed3d0ea285e895e418d
Maria Popa <49620476+marypopa@users.noreply.github.com>
2019-05-22 22:56:52 +0300

    Add files via upload

---
+++ Vorbereitung3.m
@@ -0,0 +1,12 @@
+﻿%Taylor polinom
+x0=0;
+f=[-1 0 0 6];
+t=0;
+n=[zeros(1,length(f)-1),f(1)];
+for i=2:length(f)
+    t=poly(zeros(1,i-1))
+    p=f(i)/factorial(i-1)
+    n=n+[zeros(1,length(f)-length(t)),t*p]
+endfor
+n
+    
