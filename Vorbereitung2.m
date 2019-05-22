b44f241c574287d9574fa45ddc294fef3dd052db
Maria Popa <49620476+marypopa@users.noreply.github.com>
2019-05-22 18:58:28 +0300

    Update Vorbereitung2.m

--- Vorbereitung2.m
+++ Vorbereitung2.m
@@ -1,19 +1,19 @@
-﻿X=[0 0.5 1];
+clear all;
+X=[0 0.5 1];
 f=@(x) (sin(pi*x));
-d = NaN(length(x),length(x)+1);
+d = NaN(length(X),length(X)+1);
 %Pun pe prima coloana x si pe a doua f(x)
-for i=1:length(x)
-    d(i,1) = x(i);
-    d(i,2) = v(i);
+for i=1:length(X)
+    d(i,1) = X(i);
+    d(i,2) = f(X(i));
 endfor
 
 %Incep sa construiesc
 k=1;
-for j=3:length(x)+1
-    for i=1:length(x)-k
+for j=3:length(X)+1
+    for i=1:length(X)-k
        d(i,j) =(d(i+1,j-1)-d(i,j-1))/(d(i+k,1) - d(i,1));     
     endfor
 k = k+1;
 endfor
 d
-
