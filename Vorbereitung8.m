aa3bb5c7e542c260b6cd68939d7d668fc35128da
Maria Popa <49620476+marypopa@users.noreply.github.com>
2019-05-22 22:58:43 +0300

    Add files via upload

---
+++ Vorbereitung8.m
@@ -0,0 +1,15 @@
+﻿n=6;
+for i=0:n
+  if i==0
+    x=[1];
+  else 
+    x=[1,zeros(1,i)]
+   endif
+  v = (-1)^(n-i)* poly(ones(1,n-i));
+  k = conv(x,v) * nchoosek(n,i);
+  X = [-1:0.1:1];
+  plot(polyval(k,X));
+  hold on;
+endfor
+
+
